3bf48b984bdf317fcb5e8e2e81c1e0b5d2edc93f
Hyper-W <101781346+Hyper-W@users.noreply.github.com>
2023-08-13 04:08:39 +0900

    FirstCommit

---
+++ DeployExample.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Apply Oracle Database
+kubectl apply -f ./OracleDatabase/OracleNS.yml
+kubectl apply -f ./common/CommonConfig.yml
+kubectl apply -f ./OracleDatabase/OracleDatabaseSecretExample.yml
+# kubectl apply -f ./OracleDatabase/OracleDatabaseSecret.yml
+kubectl apply -f ./OracleDatabase/OracleDatabase.yml
+
+# Apply Ords
+# kubectl apply -f ./ords/OrdsConfig.yml
+kubectl apply -f ./ords/OrdsSecretExample.yml
+# kubectl apply -f ./ords/OrdsSecret.yml
+kubectl apply -f ./ords/Ords.yml
+# kubectl apply -f ./ords/OrdsIngress.yml
